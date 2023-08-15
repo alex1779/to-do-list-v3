e760b2ea98dfbd47fcc863fb1784e763319ab8bc
alex1779 <alex1779@gmail.com>
2023-08-15 19:04:31 -0300

    Fixing errors required

--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
 import './style.css';
-import {
-  addEventsToLabels, addEventsCheckBoxes, addEventsNewTasks, addEventclearAllButton,
-  deleteFromIcons,
-} from './utils.js';
 
-window.onload = () => {
-  let tasksArray = [];
-  let savedFormData = localStorage.getItem('tasksList');
-  savedFormData = JSON.parse(savedFormData);
-  if (savedFormData) {
-    tasksArray = savedFormData;
-    const listContainer = document.querySelector('.todo-list');
-    tasksArray.forEach((todo) => {
-      listContainer.innerHTML += `<li>
-      <div class="taskCont">
-        <input type="checkbox" class="checkBoxesTasks" id="checkbox${todo.index}">
-        <div class="task" id="taskText${todo.index}">${todo.description}</div>
-        <textarea class="textArea" maxlength="255"></textarea>
-      </div>
-      <div class="icon">&#8942</div>
-    </li>
-  `;
-    });
-  }
+const tasks = [
+  {
+    index: 0,
+    completed: false,
+    description: 'review this project',
+  },
+  {
+    index: 1,
+    completed: true,
+    description: 'aprobe this project',
+  },
+];
 
-  addEventsToLabels();
-  addEventsCheckBoxes();
-  addEventsNewTasks(tasksArray);
-  addEventclearAllButton();
-  deleteFromIcons();
+const fillList = () => {
+  const container = document.querySelector('.todo-list');
+  tasks.forEach((todo) => {
+    container.innerHTML += `<li>
+    <div class="taskCont">
+      <input type="checkbox">
+      <div class="task">${todo.description}</div>
+    </div>
+    <div class="icon">&#8942</div>
+  </li>
+`;
+  });
 };
+fillList();
